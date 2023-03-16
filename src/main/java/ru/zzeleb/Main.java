2a10fc06e55ee58a002d52b797f7a5e1eff976c4
Artem Zholobov <zzeleb@gmail.com>
2023-03-16 16:08:15 +0400

    added 2 part hw

--- src/main/java/ru/zzeleb/Main.java
+++ src/main/java/ru/zzeleb/Main.java
@@ -3,9 +3,9 @@ package ru.zzeleb;
 public class Main {
     public static void main(String[] args) {
 
-        /// математические операторы + переполнение
+        /// 1.1 математические операторы + переполнение
         int result = 7 - 2;
-        System.out.println("### 1.substraction: " + result);
+        System.out.println("### 1.subtraction: " + result);
 
         int result2 = 7 + 2;
         System.out.println("### 2.addition: " + result2);
@@ -22,7 +22,7 @@ public class Main {
         int result6 = 300000 * 200000;
         System.out.println("### 6.overflow:" + result6);
 
-        // логические
+        // 1.2 логические
 
         if (result2 > result && result4 < result3) {
             System.out.println("### 7.correct");
@@ -32,6 +32,19 @@ public class Main {
         }
         if (!(result2 < result && result4 > result3)) {
             System.out.println("### 9.correct vice versa");
+
+        // 2 вычисления комбинаций типов данных (int и double)
+        int a = 500;
+        double b = 2.2;
+        double result7 = a * b;
+            System.out.println("### 10.combination:" + result7);
+            // или
+            System.out.println("### 10.2 combination:" + a * b);
+
+        double result8 = a / b;
+                   System.out.println("### 11.int/double division:" +result8);
+
+
         }
     }
 }
